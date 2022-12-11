afd68c722c5a3bec5e6c6114296ce099fa20f7ae
Casey Stango <casey.stango@gmail.com>
2022-12-10 20:05:25 -0800

    Update MArker

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -17,7 +17,9 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
         //);
 
 //  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+L.circle([34.0522, -118.2437], {
+    radius: 100
+ }).addTo(map);
 
 
 // We create the tile layer that will be the background of our map.
